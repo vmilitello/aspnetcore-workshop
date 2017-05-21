128b2af0a4f351679d19b48f4990c359a5171e78
Vincenzo Militello <v.militello@902software.com>
2017-05-21 11:06:11 -0400

    Lab 2

---
+++ Labs/Lab2/MVC/Ex2/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Ex2.Controllers
+{   
+
+    public class HomeController
+    {
+        [HttpGet("/")]
+        public string Index() => "Hello from MVC!";
+    }
+}
