c212116ac9fe62ceaa4910f875423eafe79f8adf
Vincenzo Militello <v.militello@902software.com>
2017-05-21 14:11:48 -0400

    Playin with TagHelper (Lab6)

---
+++ Labs/Lab6/TagHelpers/HelloTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.TagHelpers
+{
+    public class HelloTagHelper : TagHelper
+    {
+
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"<label>Well, Hello {Name}!</label>");
+        }
+
+    }
+}
