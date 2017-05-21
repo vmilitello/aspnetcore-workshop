c0cf9637ab414f7260b1ad3a8af040acebe252cf
Vincenzo Militello <v.militello@902software.com>
2017-05-21 12:02:00 -0400

    Lab 3 completed

--- Labs/Lab3/Startup.cs
+++ Labs/Lab3/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,11 +13,21 @@ namespace Lab3
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
+
+        public IConfigurationRoot Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -31,7 +42,7 @@ namespace Lab3
             //app.UseFileServer(); //It show directly the index.html and skip the app.Run write Async
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"Hello World from {env.EnvironmentName}!");
+                 await context.Response.WriteAsync($"{Configuration["message"]}");
             });
         }
     }
