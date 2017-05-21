ddc77e319150ecf5d65c2458d695ec5f6cde108a
Vincenzo Militello <v.militello@902software.com>
2017-05-21 11:05:51 -0400

    Lab 1

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+namespace newapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .Configure(app=> {
+                app.Run(context=> context.Response.WriteAsync("Hi dude"));
+            })
+            .UseUrls("http://localhost:5050")
+            .Build();
+            //Console.WriteLine("Hello World!");
+            host.Run();
+        }
+    }
+}
